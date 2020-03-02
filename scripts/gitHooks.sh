091694bf524547446610e9fd1b60138ef4e367c6
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-03-02 12:58:06 +0530

    |#4|noora| added script to check commit message template
    Added a script in scripts directory that will check the commit msg.If it
    is not in the correct format it will prevent commit

--- scripts/gitHooks.sh
+++ scripts/gitHooks.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-cat <<EOF > .git/hooks/pre-commit  
+cat <<EOF > .git/hooks/pre-commit
 npm run test
-if [ \$? != 0 ]; then 
+if [ \$? != 0 ]; then
     echo "\n \n check tests ...\nThere are some failing tests...\n"
     exit 1
 fi
 
 grep -rnw "//" ./src/*.js ./test/*.js *.js
-if [ \$? == 0 ]; then 
+if [ \$? == 0 ]; then
     echo "\n\n Clear your comments before commiting....\n\n"
     exit 1
 fi
 EOF
 
-chmod +x .git/hooks/pre-commit  
+chmod +x .git/hooks/pre-commit
+
+cat <<EOF > .git/hooks/commit-msg
+
+test "" = "$(grep '^|#[0-9]\+|.\+|.' "$1")" && {
+   echo 'commit message should be in this format : |#issueNumber|name| commit msg'
+   exit 1
+}
+
+exit 0
+
+EOF
+
+chmod +x .git/hooks/commit-msg
