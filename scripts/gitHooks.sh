ef06bd2c83bbfd05ff76085916407a967b8e6a13
nooranasrin <58026892+nooranasrin@users.noreply.github.com>
2020-03-03 16:39:22 +0530

    |#4|noora/sudipta| fixed a bug in the git hook
    previously there was a bug in  commit-msg hook.Fixed that bug

--- scripts/gitHooks.sh
+++ scripts/gitHooks.sh
@@ -1,30 +1,26 @@
-#!/bin/bash
+!/bin/bash
 
 cat <<EOF > .git/hooks/pre-commit
 npm run test
 if [ \$? != 0 ]; then
-    echo "\n \n check tests ...\nThere are some failing tests...\n"
-    exit 1
+   echo "\n \n check tests ...\nThere are some failing tests...\n"
+   exit 1
 fi
 
 grep -rnw "//" ./src/*.js ./test/*.js *.js
 if [ \$? == 0 ]; then
-    echo "\n\n Clear your comments before commiting....\n\n"
-    exit 1
+   echo "\n\n Clear your comments before commiting....\n\n"
+   exit 1
 fi
 EOF
 
 chmod +x .git/hooks/pre-commit
 
-cat <<EOF > .git/hooks/commit-msg
-
-test "" = "$(grep '^|#[0-9]\+|.\+|.' "$1")" && {
-   echo 'commit message should be in this format : |#issueNumber|name| commit msg'
-   exit 1
-}
-
-exit 0
-
-EOF
+echo "cat \$1 | head -1 | grep -E '^\|#[0-9]+\|[A-Za-z/]+\|.+$'
+    matchResult=\${?}
+    if (( \$matchResult != 0 )); then
+        echo \"Please Use this format for commit message \n|#<issueNo>|<pair1Name>/<pair2Name>|commit head\nexample: |#001|denis/ritche|commitHead\"
+    fi
+    exit \$matchResult" > .git/hooks/commit-msg
 
 chmod +x .git/hooks/commit-msg
