5fd5eeee8dd437d263dc529b660723c4583d2fa7
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-03-06 20:28:52 +0530

    |#8|Shivam/Satheesh|Added limit in request
    Added 100kb limit in request of server

--- src/router.js
+++ src/router.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+app.use(express.json({limit: '100kb'}));
 
 module.exports = {app};
