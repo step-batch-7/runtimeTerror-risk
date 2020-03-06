c63b322d5cb2eae85bec348877a08ac60a2c2aba
santhosh <santhosh.kumar@thoughtworks.com>
2020-03-06 19:28:53 +0530

    |#9|santhosh/rashmi| modified game class constructor

--- src/game.js
+++ src/game.js
@@ -1,13 +1,11 @@
 class Game {
   #countries;
   #players;
-  #currentState;
   #currentPlayer;
   constructor(countries) {
     this.#countries = countries;
     this.#players = [];
-    this.#currentState = {};
-    this.#currentPlayer = {};
+    this.#currentPlayer;
   }
 }
 
