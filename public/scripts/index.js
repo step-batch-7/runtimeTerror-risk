c72c8d91ddf477c6c6135caeffb3475bcb85a1c6
santhosh <santhosh.kumar@thoughtworks.com>
2020-03-07 20:30:14 +0530

    |#9|santhosh/rashmi|added popup in frontend show popup on territory on click if already claimed

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,44 +1,57 @@
+const mousePointerPopUp = function(event, msg) {
+  const $mousePopUp = getElement('#mouse-pop-up');
+  $mousePopUp.classList.remove('hidden');
+  $mousePopUp.innerText = msg;
+  $mousePopUp.style.top = `${event.clientY + 20}px`;
+  $mousePopUp.style.left = `${event.clientX}px`;
+  setTimeout(() => {
+    $mousePopUp.classList.add('hidden');
+  }, 1500);
+};
+
 const showReinforcementStatus = function({ status, message }) {
   if (!status && message) {
     return;
   }
 };
 
-const sendReinforcementRequest = function(territory, militaryCount = 1) {
+const sendReinforcementRequest = function(event, militaryCount = 1) {
   fetch('/reinforcement', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ territory, militaryCount })
+    body: JSON.stringify({ territory: event.target.id, militaryCount })
   })
     .then(response => response.json())
     .then(showReinforcementStatus);
 };
 
-const updateTerritory = function(response, territory) {
+const updateTerritory = function(response, event) {
   if (response.status) {
-    getElement(`#${territory}`).style.fill = 'red';
-    getElement(`#${territory} + .unit`).innerHTML = '&nbsp;1';
+    getElement(`#${event.target.id}`).style.fill = 'red';
+    getElement(`#${event.target.id} + .unit`).innerHTML = '&nbsp;1';
+    return;
   }
+  mousePointerPopUp(event, response.error);
 };
 
-const sendClaimRequest = function(territory) {
+const sendClaimRequest = function(event) {
   fetch('/claimTerritory', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ territory })
+    body: JSON.stringify({ territory: event.target.id })
   })
     .then(response => response.json())
-    .then(data => updateTerritory(data, territory));
+    .then(data => updateTerritory(data, event));
 };
 
 const selectListener = function() {
   const listeners = { '1': sendClaimRequest, '2': sendReinforcementRequest };
   const stage = localStorage.getItem('stage');
-  listeners[stage](event.target.id);
+  listeners[stage](event);
 };
 
 const addListenerOnterritory = () => {
