d5247c67d05d436b5beea66e3433589633df9af4
santhosh <santhosh.kumar@thoughtworks.com>
2020-03-09 17:48:18 +0530

    |#10|santhosh/rashmi|modified styles of activity log
    modified player names

--- src/router.js
+++ src/router.js
@@ -1,30 +1,21 @@
 const express = require('express');
-const {
-  getGameStatus,
-  performClaim,
-  performReinforcement,
-  hasFields
-} = require('./handlers');
+const { getGameStatus, performClaim, performReinforcement, hasFields } = require('./handlers');
 const Game = require('./game');
 const generateTerritories = require('./territories');
 const app = express();
 
 const territories = generateTerritories();
 const game = new Game(territories);
-game.addPlayer('Player1');
-game.addPlayer('Player2');
-game.addPlayer('Player3');
+game.addPlayer('Rashmi');
+game.addPlayer('Noora');
+game.addPlayer('Santhosh');
 
 app.locals.game = game;
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json({limit: '100kb'}));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '100kb' }));
 app.get('/gameStatus', getGameStatus);
-app.post(
-  '/reinforcement',
-  hasFields('territory', 'militaryCount'),
-  performReinforcement
-);
+app.post('/reinforcement', hasFields('territory', 'militaryCount'), performReinforcement);
 app.post('/performClaim', hasFields('territory'), performClaim);
 
-module.exports = {app};
+module.exports = { app };
